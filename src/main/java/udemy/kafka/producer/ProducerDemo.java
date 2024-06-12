f1874b48b3008ce574f2866c785d3a8a60aad471
anish <anish.diliyan@gmail.com>
2024-06-12 09:52:46 +0530

    producer created and message pushed

---
+++ src/main/java/udemy/kafka/producer/ProducerDemo.java
@@ -0,0 +1,46 @@
+package udemy.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static final Logger log =
+            LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Producer started.....");
+
+        // create producer basic properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+        // create key and value serializer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create producer record (topic with message that we want to send)
+        ProducerRecord<String, String> producerRecord= new ProducerRecord<>(
+          "demo_java", "Hello kafka, I am Java producer"
+        );
+
+        // send data (topic demo_java should exists)
+        producer.send(producerRecord);
+
+        // flush: tell the producer to send all the data and block until done
+        // -- synchronized operation
+        producer.flush();
+
+        // close: this will flush and then close the producer
+        // used flush before just for understanding, we can only do close
+        producer.close();
+    }
+}
